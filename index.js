84e0a24f55e270bbfabf351ac0ab3319e875748b
Tyler Brock <tyler.brock@gmail.com>
2016-11-20 13:52:42 -0800

    Cant use say twice

--- index.js
+++ index.js
@@ -81,8 +81,7 @@ app.post('/process', function (req, res) {
 app.post('/call', function (req, res) {
   var twiml = new twilio.TwimlResponse();
   twiml
-    .say('Welcome to hello guv')
-    .say(describeIssues())
+    .say(['Welcome to hello guv', describeIssues()].join(' '))
     .gather({
       numDigits: 1,
       action: '/process',
